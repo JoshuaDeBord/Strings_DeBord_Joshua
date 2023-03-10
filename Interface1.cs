fd89981c197980e660214c8caf70d462f3ea0620
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-03-10 13:16:21 -0600

    Initial commit

---
+++ Interface1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings_DeBord_Joshua
+{
+    interface Interface1
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+
+namespace Strings_DeBord_Joshua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a character");
+            char myChar = Console.ReadKey().KeyChar;
+
+            bool isLetter = Char.IsLetter(myChar);
+
+            Console.WriteLine($"\n {myChar} is a letter: {isLetter}.");
+        }
+    }
+}
