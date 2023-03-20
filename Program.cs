02be4efbf09136f584873d028f00677bdb33cfef
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-03-20 12:46:25 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,17 @@ namespace Strings_DeBord_Joshua
             bool isLetter = Char.IsLetter(myChar);
 
             Console.WriteLine($"\n {myChar} is a letter: {isLetter}.");
+
+            Console.WriteLine("Why do you like video games?");
+            string videoGameQuestion = Console.ReadLine();
+
+            Console.WriteLine("Please input a word.");
+            string word = Console.ReadLine();
+
+            bool isIn = videoGameQuestion.Contains(word);
+
+            Console.WriteLine($"{word} exists in {videoGameQuestion}, {isIn}!");
+
         } 
     }
 }
